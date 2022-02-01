78f69d6832833c04f1fd2d3f3262f3455976e93b
Marijke Drenth <drenth.marijke@googlemail.com>
2022-02-01 18:44:42 +0200

    Add library tests and class

---
+++ src/main/java/Library.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Library {
+
+    private ArrayList<Book> listOfBooks;
+    private int capacity;
+
+    public Library(int capacity){
+        this.capacity = capacity;
+        listOfBooks = new ArrayList<Book>();
+    }
+
+    public int getBookCount() {
+        return this.listOfBooks.size();
+    }
+
+    public void addBook(Book book1) {
+        if (this.hasSpace()) this.listOfBooks.add(book1);
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public boolean hasSpace(){
+        return this.capacity > this.listOfBooks.size();
+    }
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,45 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryTest {
+
+    private Library library;
+    private Book book1;
+    private Book book2;
+    private Book book3;
+
+    @Before
+    public void before(){
+        library = new Library(5);
+        book1 = new Book("Lord of the Rings", "JRR Tolkien", "Fantasy");
+        book2 = new Book("Harry Potter", "JK Rowling", "Fantasy");
+        book3 = new Book("It", "Stephen King", "Horror");
+    }
+
+    @Test
+    public void libraryStartsEmpty(){
+        assertEquals(0, library.getBookCount());
+    }
+
+    @Test
+    public void canAddBook(){
+        library.addBook(book1);
+        assertEquals(1, library.getBookCount());
+    }
+
+    @Test
+    public void hasCapacity(){
+        assertEquals(5, library.getCapacity());
+    }
+
+    @Test
+    public void cannotAddBookIfLibraryFull(){
+        Library fullLibrary = new Library(1);
+        fullLibrary.addBook(book1);
+        fullLibrary.addBook(book2);
+        assertEquals(1, fullLibrary.getBookCount());
+    }
+
+}
